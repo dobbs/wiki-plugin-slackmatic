e2f62cda3638a56f19179b44b8aabd5de565e284
Eric Dobbs <eric@dobbse.net>
2019-06-26 07:48:12 -0600

    bash mkplugin.js.sh Slackmatic

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+# Federated Wiki - Slackmatic Plugin
+
+This plugin, type: slackmatic, extends the markup of the federated wiki.
+
+## Build
+
+    npm install
+    grunt build
+
+## License
+
+MIT
+
---
+++ test/test.js
@@ -0,0 +1,17 @@
+// build time tests for slackmatic plugin
+// see http://mochajs.org/
+
+(function() {
+  const slackmatic = require('../client/slackmatic'),
+        expect = require('expect.js');
+
+  describe('slackmatic plugin', () => {
+    describe('expand', () => {
+      it('can make itallic', () => {
+        var result = slackmatic.expand('hello *world*');
+        return expect(result).to.be('hello <i>world</i>');
+      });
+    });
+  });
+
+}).call(this);
