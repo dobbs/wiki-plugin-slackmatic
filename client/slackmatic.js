8fa1776dd530d6a384cb43118f169634160e1ec1
Eric Dobbs <eric@dobbse.net>
2019-08-02 15:03:27 -0400

    Sketch slackmatic editor

--- client/slackmatic.js
+++ client/slackmatic.js
@@ -1,6 +1,42 @@
+"use strict";
 
 (function() {
   var bind, emit, expand;
+
+  const editorTemplate = document.createElement('template')
+  editorTemplate.innerHTML = `
+  <style>
+   div { margin-bottom: 1em; padding: 15px; background-color: #eee; }
+   input { box-sizing: border-box; width: 100%; }
+  </style>
+  <div>
+    <form>
+    <input name="hello" type="text" placeholder="say anything" value="" />
+    <input type="submit" value="save" />
+    </form>
+  </div>`
+  class SlackmaticEditor extends HTMLElement {
+    constructor() {
+      super()
+      this.attachShadow({mode: 'open'})
+      this.shadowRoot.appendChild(editorTemplate.content.cloneNode(true))
+    }
+
+    connectedCallback() {
+      if (super.connectedCallback)
+        super.connectedCallback()
+      console.log({
+        THIS: this,
+        shadowRoot: this.shadowRoot,
+        querySelector: this.shadowRoot.querySelector
+      })
+      this.shadowRoot.querySelector('form').addEventListener('submit', event => {
+        event.preventDefault()
+        event.stopPropagation()
+        console.log('save', {event})
+      })
+    }
+  }
 
   expand = text => {
     return text
@@ -13,18 +49,19 @@
   emit = ($item, item) => {
     return $item.append(`
       <p style="background-color:#eee;padding:15px;">
-        ${expand(item.text)}
-      </p>`);
-  };
+        ${expand(item.text || 'intentionally blank')}
+      </p>`)
+  }
 
   bind = function($item, item) {
     return $item.dblclick(() => {
-      return wiki.textEditor($item, item);
-    });
-  };
+      $item.html('<slackmatic-editor></slackmatic-editor>')
+    })
+  }
 
   if (typeof window !== "undefined" && window !== null) {
-    window.plugins.slackmatic = {emit, bind};
+    customElements.define('slackmatic-editor', SlackmaticEditor)
+    window.plugins.slackmatic = {emit, bind, SlackmaticEditor};
   }
 
   if (typeof module !== "undefined" && module !== null) {
