0bcb650745f1acd232f85c622f0d59e3dd279430
Eric Dobbs <eric@dobbse.net>
2019-08-02 11:43:00 -0400

    Proxy api requests for users.conversations & conversations.history

--- server/server.js
+++ server/server.js
@@ -2,16 +2,40 @@
 // These handlers are launched with the wiki server.
 
 (function() {
-  const startServer = params => {
-    var app = params.app,
-        argv = params.argv;
+  const slack = require('slack')
 
-    return app.get('/plugin/slackmatic/:thing', (req, res) => {
-      var thing = req.params.thing;
-      return res.json({thing});
-    });
-  };
+  const startServer = async ({app, argv}) => {
 
-  module.exports = {startServer};
+    app.post('/plugin/slackapi/echo', (req, res) => {
+      let {body} = req
+      return res.json({body})
+    })
 
-}).call(this);
+    app.post('/plugin/slackapi/users.conversations', async (req, res) => {
+      let {token}  = req.body
+      try {
+        let slackres = await slack.users.conversations({token})
+        return res.json(slackres)
+      } catch (err) {
+        return res.json({err})
+      }
+    })
+
+    app.post('/plugin/slackapi/conversations.history', async (req, res) => {
+      let {token, channel, oldest, latest, title, cursor}  = req.body
+      let slackres
+      try {
+        slackres = await slack.conversations.history({
+          token, channel, oldest, latest, cursor /* TODO how do we handle cursor? */
+        })
+        return res.json(slackres)
+      } catch (err) {
+        return res.json({err, response: slackres})
+      }
+    })
+
+  }
+
+  module.exports = {startServer}
+
+}).call(this)
