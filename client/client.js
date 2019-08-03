fee9700c9269acd8e6bc51c11d860a767ada437f
Eric Dobbs <eric@dobbse.net>
2019-08-03 17:12:22 -0400

    Remove custom elements 'cos shadow DOM interfered with input focus
    Don't entirely understand the behavior, but it works better without
    the custom elements.
    
    Also expose editor in the plugin for use by the factory plugin

--- client/client.js
+++ client/client.js
@@ -1,57 +1,30 @@
 "use strict";
 
 (function() {
-  var bind, emit, expand;
+  var bind, emit, expand, template, editor;
 
-  const registerCustomElements = (document, customElements) => {
-    const CryptoJS = require('crypto-js')
+  const CryptoJS = require('crypto-js')
 
-    const editorTemplate = document.createElement('template')
-    editorTemplate.innerHTML = `
-    <style>
-     div { margin-bottom: 1em; padding: 15px; background-color: #eee; }
-     input { box-sizing: border-box; width: 100%; }
-    </style>
-    <div>
+  const createTemplate = document => {
+    const display = document.createElement('template')
+    display.innerHTML = `
+    <section style="margin-bottom: 1em; padding: 15px; background-color: #eee;">
+      <div>Slackmatic Drop Zone</div>
+      <div data-id="message"></div>
+    </section>`
+
+    const editor = document.createElement('template')
+    editor.innerHTML = `
+    <section style="margin-bottom: 1em; padding: 15px; background-color: #eee;">
       <form>
-      <input name="hello" type="text" placeholder="say anything" value="" />
-      <input type="submit" value="save" />
+      <input name="token" type="text" placeholder="slackbot token" value=""
+             style="box-sizing: border-box; width: 100%;" />
+      <input type="submit" value="save"
+             style="box-sizing: border-box; width: 100%;" />
       </form>
-    </div>`
-    class SlackmaticEditor extends HTMLElement {
-      constructor() {
-        super()
-        this.wikiItem = {}
-        this.attachShadow({mode: 'open'})
-        this.shadowRoot.appendChild(editorTemplate.content.cloneNode(true))
-      }
+    </section>`
 
-      set item (obj) { this.wikiItem = obj }
-      get item () { return this.wikiItem }
-      set saveHandler(fn) { this.save = fn }
-      get saveHandler() { return this.save }
-
-      connectedCallback() {
-        if (super.connectedCallback)
-          super.connectedCallback()
-        if (this.item.hello)
-          this.shadowRoot.querySelector('input[name=hello]').value = this.item.hello
-        const form = this.shadowRoot.querySelector('form')
-        form.addEventListener('submit', event => {
-          event.preventDefault()
-          event.stopPropagation()
-          const newValues = Array.from(form.elements).reduce((all, item) => {
-            if (item.name)
-              all[item.name] = item.value
-            return all
-          }, {})
-          const newItem = Object.assign({}, this.item, newValues)
-          this.save(newItem)
-        })
-      }
-    }
-    customElements.define('slackmatic-editor', SlackmaticEditor)
-    return SlackmaticEditor
+    return  {display, editor}
   }
 
   expand = text => {
@@ -63,30 +36,40 @@
   };
 
   emit = ($item, item) => {
-    return $item.append(`
-      <p style="background-color:#eee;padding:15px;">
-        ${expand(`text: ${item.text ||'intentionally blank'} hello: ${item.hello || 'null'}`)}
-      </p>`)
+    $item.html(template.display.content.cloneNode(true))
+    $item.find('[data-id=message]').html(expand(item.token || ''))
   }
 
-  bind = function($item, item) {
-    return $item.dblclick(() => {
-      const editor = document.createElement('slackmatic-editor')
-      editor.item = item
-      editor.saveHandler = newItem => {
-        wiki.pageHandler.put($item.parents('.page:first'), {
-          type: 'edit',
-          id: newItem.id,
-          item: newItem
-        })
-      }
-      $item.html(editor)
+  editor = ($item, item) => {
+    $item.html(template.editor.content.cloneNode(true))
+    const form = $item.find('form')[0]
+    form.token.value = item.token || ''
+    form.addEventListener('submit', event => {
+      event.stopPropagation()
+      event.preventDefault()
+      const newEntries = Array.from(form.elements).reduce((all, input) => {
+        if (input.name)
+          all[input.name] = input.value
+        return all
+      }, {})
+      const newItem = Object.assign({}, item, newEntries)
+      wiki.pageHandler.put($item.parents('.page:first'), {
+        type: 'edit',
+        id: newItem.id,
+        item: newItem
+      })
+      emit($item, newItem)
+      bind($item, newItem)
     })
   }
 
+  bind = function($item, item) {
+    $item.dblclick(() => { editor($item, item) })
+  }
+
   if (typeof window !== "undefined" && window !== null) {
-    let SlackmaticEditor = registerCustomElements(document, customElements)
-    window.plugins.slackmatic = {emit, bind, SlackmaticEditor};
+    template = createTemplate(document)
+    window.plugins.slackmatic = {emit, bind, editor}
   }
 
   if (typeof module !== "undefined" && module !== null) {
